76ad23043b9c377b4edc57d10b736bb1424e0106
Jungkyu Yang <didwjd8590412@gmail.com>
2025-07-02 20:06:43 +0900

    feat(constants): 프로젝트/조직/기술스택 등 하드코딩 값 constants로 분리

---
+++ app/constants/projectMeta.js
@@ -0,0 +1,78 @@
+export const PROJECT_META = {
+  "4yclinghomerun-client": {
+    title: "KTwiz홈페이지 UI/UX 개선 프로젝트",
+    customDescription:
+      "기존 KTwiz 홈페이지의 UI/UX를 개선한 웹 사이트로서, SEO 개선 및 사용자 편의성 개선에 목적을 둔 기업 연계 프로젝트입니다.",
+    customCreatedAt: "2024-09-02",
+    customUpdatedAt: "2024-09-27",
+    techStack: [
+      "TypeScript",
+      "React",
+      "TanStack-Query",
+      "Axios",
+      "TailwindCSS",
+      "Vercel",
+      "Framer-Motion",
+    ],
+  },
+  "pre-onboarding-12th-1-18": {
+    title: "투두리스트",
+    customDescription:
+      "로그인·회원가입·투두리스트를 구현한 웹 사이트로서 REST API 활용 및 UX 개선에 목적을 두었습니다.",
+    customCreatedAt: "2023-08-21",
+    customUpdatedAt: "2023-08-25",
+    techStack: ["TypeScript", "React", "Emotion", "AWSamplify"],
+  },
+  "pre-onboarding-12th-2-18": {
+    title: "무한스크롤 리스트",
+    customDescription:
+      "리스트 및 광고를 노출시키기 위해 무한 스크롤을 활용한 웹 사이트로서, useIntersectionObserver 커스텀 훅을 활용 및 UX 개선에 목적을 두었습니다. ",
+    customCreatedAt: "2023-08-28",
+    customUpdatedAt: "2023-09-01",
+    techStack: ["TypeScript", "React", "Scss", "Amazon S3"],
+  },
+  "pre-onboarding-12th-3-18": {
+    title: "캐싱/디바운스 검색",
+    customDescription:
+      "검색어 입력 시 캐시 스토리지 및 디바운스를 활용한 웹 사이트로서, API 호출 최소화 맟 UX 개선에 목적을 두었습니다.",
+    customCreatedAt: "2023-09-04",
+    customUpdatedAt: "2023-09-08",
+    techStack: ["TypeScript", "React", "Scss", "Amazon S3"],
+  },
+  "toy-project1-team2": {
+    title: "직원관리 사이트",
+    customDescription:
+      "직원관리·공지관리·근무관리·휴과관리 등 관리자가 직원을 효율적으로 관리할 수 있게 지원해주는 웹 사이트로서, 바닐라 자바스크립트로 구현하는 방법에 목적을 두었습니다.  ",
+    techStack: ["JavaScript"],
+  },
+  "toy-project2-team4": {
+    title: "학원운영 사이트",
+    customDescription:
+      "로그인·스케쥴 관리·급여 관리 등 관리자가 학원을 효율적으로 관리할 수 있게 지원해주는 웹 사이트로서, Next.js 프레임워크 사용법을 익히는데 목적을 두었습니다.",
+    techStack: [
+      "JavaScript",
+      "React",
+      "Next.js",
+      "Redux",
+      "Scss",
+      "Msw(Mocking)",
+    ],
+  },
+  "toy-project3-team1": {
+    title: "영상 공유 커뮤니티 모바일 플랫폼",
+    customDescription:
+      "로그인·영상 등록·썸네일 업로드·프로필 설정·댓글·좋아요·구독 등 게이머 사용자들이 자신만의 Youtube 영상 리스트를 등록하고 소통하는 커뮤니티 플랫폼으로서, 의존성이 분리된 코드 구현 및 UX 개선에 목적을 두었습니다.",
+    techStack: [
+      "TypeScript",
+      "React",
+      "TanStack-Query",
+      "Zustand",
+      "Shadcn",
+      "TailwindCSS",
+      "Playwright",
+      "Supabase",
+      "Netlify",
+      "Framer-Motion",
+    ],
+  },
+};
